93b6bbc0cde14b972acb617e0822623d66114e83
script-robloxhack <helovn3041975@gmail.com>
2025-07-03 12:38:43 +0700

    Da hood.lua

---
+++ Da hood.lua
@@ -0,0 +1,50 @@
+-- Copy and paste everything from the button "Copy" or selecting it all.
+
+
+
+--Video: https://youtu.be/G7D0cbWUV-U
+--------
+--For support, join the script's  server: https://discord.gg/stando
+--------
+--For additional support, join my server: 	https://discord.gg/M7UTUtwGkJ
+
+getgenv()._=
+"Join discord.gg/msgabv2t9Q | discord.gg/stando to get latest update ok bai +_.| If you pay for this script you get scammed, this script is completely free ok"
+
+getgenv().Config = {
+    ['AntiMod'] = true,
+    ['AntiRpg'] = true,
+    ['AntiSeat'] = true,
+    ['BulletTp'] = true,
+    ['AutoMask'] = false,
+    ['MaskMode'] = 'Surgeon', --[ Surgeon | Paintball | Skull | Pumpkin | Hockey | Ninja | Riot | Breathing ]
+    ['NeckGrab'] = false,
+    ['GrabMode'] = 'Neck', -- [ Neck | Air ]
+    ['HitNotif'] = true, -- [Notify when someone hit you]
+    ['AutoCsync'] = true, --[ Csync to target when using bullet tp ]
+    ['AutoArmor'] = false,
+    ['AutoBlock'] = true,
+    ['AntiStompV1'] = true,
+    ['AntiStompV2'] = false,
+    ['TargetMode'] = 'Hide', --[ Random | Hide ]
+    ['BulletTpMode'] = 'Hide', -- [ Default | Hide | Randomize ]
+    ['SelfHighlight'] = true,
+    ['VoidDesyncMode'] = 'Random', -- [ Random | Idle | Up N Down | Side ]
+    ['RespawnLocation'] = false,
+    ['SilentAnimation'] = true,
+    ['Keybind'] = {
+        ['CFrame'] = {"c", true},
+        ['Fly'] = {"x", false},
+        ['IronManFly'] = {"x", false},
+        ['VehicleFly'] = {"x", false},
+        ['VoidDesync'] = {"t", true},
+        ['FakeMacro'] = {"e", false},
+        ['QuickGrab'] = {"r", true},
+        ['QuickStomp'] = {"e", true},
+        ['GuiKeybind'] = {"v", true},
+        ['Grenade'] = {"x", false},
+        ['TearGas'] = {"x", false},
+        ['Flashbang'] = {"x", false},
+        
+    }
+}loadstring(game:HttpGet("https://xk5ng.github.io/XK5NG"))()
